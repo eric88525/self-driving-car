579596a477495aa5c8817f5aff77bb22fd5e5340
eric885251 <eric88525@gmail.com>
2022-08-27 17:37:44 +0800

    First commit - Add car - Add road

---
+++ car.js
@@ -0,0 +1,78 @@
+class Car {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
+        this.controls = new Controls();
+    }
+
+    update() {
+        this.#move();
+    }
+
+    #move() {
+        if (this.controls.forward) {
+            this.speed += this.acceleration;
+        }
+        if (this.controls.reverse) {
+            this.speed -= this.acceleration;
+        }
+        // limit max speed
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        // let car reverse not this fast
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
+        }
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        }
+        if (this.speed < 0) {
+            this.speed += this.friction;
+        }
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
+            }
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
+            }
+        }
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+    }
+
+    draw(ctx) {
+        ctx.save()
+        // Remaps the (0,0) position on the canvas
+        ctx.translate(this.x, this.y);
+
+        ctx.rotate(-this.angle);
+        // Begins a path, or resets the current path
+        ctx.beginPath();
+        // x, y, rectangle width, rectangle height
+        ctx.rect(
+            -this.width / 2,
+            -this.height / 2,
+            this.width,
+            this.height
+        );
+        // Fills the current drawing (path)
+        ctx.fill();
+
+        ctx.restore();
+    }
+}
---
+++ main.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById("myCanvas");
+canvas.width = 200;
+
+
+const ctx = canvas.getContext("2d");
+
+// objects
+const road = new Road(canvas.width / 2, canvas.width);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+
+car.draw(ctx);
+animate();
+
+function animate() {
+    car.update();
+    canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height*0.5);
+
+    road.draw(ctx);
+    car.draw(ctx);
+
+    ctx.restore();
+    requestAnimationFrame(animate);
+}
